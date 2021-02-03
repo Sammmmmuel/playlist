1e4c9376936e89c9e5852bee5bdd8adcd0b6c29d
Sammmmmuel <69543188+Sammmmmuel@users.noreply.github.com>
2021-02-02 22:41:08 -0800

    working on playlist.py

--- Playlist.py
+++ Playlist.py
@@ -8,20 +8,45 @@ class Playlist:
   # TODO: Create a method called add_song that creates a Song object and adds it to the playlist. This method has one parameter called title.
 
   def add_song(self, title):
-    pass
+    new_song = Song(title)
+    new_song.set_title(title)
+    new_song.set_next_song(self.__first_song)
+    self.__first_song = new_song
 
 
 
   # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index. The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index.
 
   def find_song(self, title):
-    pass
+    index = 0 
+    current_song = self.__first_song
+
+    while current_song != None:
+      if current_song.get_title == title:
+        return index
+      else:
+        index += 1
+        current_song = current_song.get_next_song()
+    return index -1
 
 
   # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
 
   def remove_song(self, title):
-    pass
+    current_song = self.__first_song
+
+    if (current_song.get_title() == title.title()):
+        self.__first_song = current_song.get_next_song()
+        return print(f'Deleted {title} from Playlist')
+
+    else: 
+      while current_song.get_title() != title:
+        if current_song.get_next_song().get_title() == title: 
+          current_song.set_next_song(current_song.get_next_song())
+          return print(f'Deleted {title} from Playlist')
+        else: 
+          current_song = current_song.get_next_song()
+
 
 
 
